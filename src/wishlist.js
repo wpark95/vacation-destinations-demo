71f92c694dc4026ccfbcf852ba05e1ffb9233173
Won Gyun Park <wongyun@amazon.com>
2022-08-22 14:47:11 -0700

    initial commit

---
+++ src/wishlist.js
@@ -0,0 +1,91 @@
+const userInputForm = document.querySelector("form");
+const userInputs = userInputForm.querySelectorAll(".user-input-box");
+const wishlist = document.querySelector("#wishlist-container");
+
+const init = () => {
+    userInputForm.addEventListener("submit", addToListHandler);
+}
+
+const addToListHandler = (e) => {
+    const userInputsArr = [];
+
+    e.preventDefault();
+    for (i = 0; i < userInputs.length; i++) {
+        userInputsArr.push(userInputs[i].value);
+        userInputs[i].value = "";
+    }
+    addToWishlist(userInputsArr[0], userInputsArr[1], userInputsArr[2], userInputsArr[3]);
+}
+
+const addToWishlist = (name, location, photo, description) => {
+    // Create container for each wichlist item
+    let listItemContainer = document.createElement("div");
+    listItemContainer.setAttribute("class", "list-item-container");
+
+    // Create container for wishlist item information
+    let listItemInfoContainer = document.createElement("div");
+
+    // Create children for wishlist item information (destination name, location, photo) to list item information container (listItemInfoContainer)
+    let listItemName = document.createElement("h3");
+    listItemName.innerText = name;
+
+    let listItemLocation = document.createElement("h5");
+    listItemLocation.innerText = location;
+
+    let listItemPhoto = document.createElement("img");
+    listItemPhoto.setAttribute("class", "list-item-image")
+    let defaultPhoto = "https://misstourist.com/wp-content/uploads/2021/03/0-best-airbnb-aruba.jpg";
+    photo.length < 1 ? listItemPhoto.setAttribute("src", defaultPhoto) : listItemPhoto.setAttribute("src", photo);
+
+    // Append the created children to list item information container
+    listItemInfoContainer.append(listItemName);
+    listItemInfoContainer.append(listItemLocation);
+    listItemInfoContainer.append(listItemPhoto);
+    if (description.length > 0) {
+        listItemDescription = document.createElement("p");
+        listItemDescription.innerText = description;
+        listItemInfoContainer.append(listItemDescription);
+    }
+
+    // Create container for edit & remove buttons
+    let listItemButtonsContainer = document.createElement("div");
+
+    // Create edit & remove buttons
+    let editButton = document.createElement("button")
+    editButton.innerText = "Edit";
+    editButton.addEventListener("click", editButtonHandler);
+    let removeButton = document.createElement("button")
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener("click", removeButtonHandler);
+
+    // Append edit & remove buttons to the created buttons container
+    listItemButtonsContainer.append(editButton);
+    listItemButtonsContainer.append(removeButton);
+    
+    listItemContainer.append(listItemInfoContainer);
+    listItemContainer.append(listItemButtonsContainer);
+
+    wishlist.append(listItemContainer);
+}
+
+const editButtonHandler = (e) => {
+    listItemContainer = e.target.parentElement.parentElement;
+    const name = listItemContainer.querySelector("h3");
+    const location = listItemContainer.querySelector("h5");
+    const photo = listItemContainer.querySelector("img");
+
+    const newName = window.prompt("Enter new name");
+    const newLocation = window.prompt("Enter new location");
+    const newPhoto = window.prompt("Enter new photo url");
+    
+    newName.length < 1 ? null : name.innerText = newName;
+    newLocation.length < 1 ? null : location.innerText = newLocation;
+    newPhoto.length < 1 ? null : photo.setAttribute("src", newPhoto);
+}
+
+const removeButtonHandler = (e) => {
+    const listItemContainer = e.target.parentElement.parentElement;
+    listItemContainer.remove();
+}
+
+init();
