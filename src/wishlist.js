f3601f40f693d567908f5bf1fd29a1d84e86389c
wpark95 <park.wongyun@gmail.com>
2022-08-23 09:23:33 -0700

    Restructure wishlist.js and add comments for functions for better readability

--- src/wishlist.js
+++ src/wishlist.js
@@ -6,18 +6,46 @@ const init = () => {
     userInputForm.addEventListener("submit", addToListHandler);
 }
 
+// Handles the submission of user inputs (e.g., name, location, photo).
 const addToListHandler = (e) => {
+    e.preventDefault();
     const userInputsArr = [];
 
-    e.preventDefault();
     for (i = 0; i < userInputs.length; i++) {
         userInputsArr.push(userInputs[i].value);
         userInputs[i].value = "";
     }
+
     addToWishlist(userInputsArr[0], userInputsArr[1], userInputsArr[2], userInputsArr[3]);
     changeWishlistTitle();
 }
 
+// Prompts the user to enter new name, location, and photo of their wishlist item that they want to edit. 
+// Uses original values if user omits new values. Otherwise, sets newly provided values to the item
+const editButtonHandler = (e) => {
+    listItemContainer = e.target.parentElement.parentElement;
+    const name = listItemContainer.querySelector(".list-item-name");
+    const location = listItemContainer.querySelector(".list-item-location");
+    const photo = listItemContainer.querySelector(".list-item-image");
+
+    const newName = window.prompt("Enter new name");
+    const newLocation = window.prompt("Enter new location");
+    const newPhotoUrl = window.prompt("Enter new photo url");
+    
+    newName.length > 0 ? name.innerText = newName : null;
+    newLocation.length > 0 ? location.innerText = newLocation : null;
+    newPhotoUrl.length > 0 ? photo.setAttribute("src", newPhotoUrl) : null;
+}
+
+// Removes the target wishlist item when user clicks the remove button
+const removeButtonHandler = (e) => {
+    const listItemContainer = e.target.parentElement.parentElement;
+    listItemContainer.remove();
+}
+
+/* 
+Helper Functions
+*/
 const addToWishlist = (name, location, photoUrl, description) => {
     // Create container for each wichlist item
     const listItemContainer = document.createElement("div");
@@ -32,6 +60,7 @@ const addToWishlist = (name, location, photoUrl, description) => {
     listItemPhoto.setAttribute("class", "list-item-image")
     const defaultPhotoUrl = "https://c.tenor.com/_4YgA77ExHEAAAAd/rick-roll.gif";
     photoUrl.length > 0 ? listItemPhoto.setAttribute("src", photoUrl) : listItemPhoto.setAttribute("src", defaultPhotoUrl);
+    console.log(photoUrl.match(/\w+\.(jpg|jpeg|gif|png|tiff|bmp)$/gi));
 
     // Create children for wishlist item information (i.e., destination name, location, and description if provided) for list item information container
     const listItemName = document.createElement("p");
@@ -71,32 +100,13 @@ const addToWishlist = (name, location, photoUrl, description) => {
     wishlist.append(listItemContainer);
 }
 
-const editButtonHandler = (e) => {
-    listItemContainer = e.target.parentElement.parentElement;
-    const name = listItemContainer.querySelector(".list-item-name");
-    const location = listItemContainer.querySelector(".list-item-location");
-    const photo = listItemContainer.querySelector(".list-item-image");
-
-    const newName = window.prompt("Enter new name");
-    const newLocation = window.prompt("Enter new location");
-    const newPhotoUrl = window.prompt("Enter new photo url");
-    
-    newName.length > 0 ? name.innerText = newName : null;
-    newLocation.length > 0 ? location.innerText = newLocation : null;
-    newPhotoUrl.length > 0 ? photo.setAttribute("src", newPhotoUrl) : null;
-}
-
-const removeButtonHandler = (e) => {
-    const listItemContainer = e.target.parentElement.parentElement;
-    listItemContainer.remove();
-}
-
-// Helper Functions
+// Updates wishlist title to become "My Wishlist!" when user adds an item to the wishlist
 const changeWishlistTitle = () => {
     wishlistTitle = document.querySelector("#wishlist-title");
     wishlistTitle.innerText = "My Wishlist"
 }
 
+// Creates an edit or remove button used for each item in the wishlist
 const createEditOrRemoveButton = (buttonType) => {
     const buttonClassName = buttonType.toLowerCase() + "-btn";
     const button = document.createElement("button");
