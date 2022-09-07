7c7a06eb9f68535384af0381d6d01502026d674f
wpark95 <park.wongyun@gmail.com>
2022-09-07 15:36:19 -0700

    Enforce parentheses around parameters styling for arrow functions

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,11 +5,11 @@ const connectionStr = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(connectionStr);
 const db = mongoose.connection;
 
-db.once('open', _ => {
+db.once('open', () => {
     console.log('Database connected.');
 });
 
-db.on('error', err => {
+db.on('error', (err) => {
     console.error('connection error: ', err);
 });
 
